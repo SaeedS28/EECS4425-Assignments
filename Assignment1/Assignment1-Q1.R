0b702375091e62941ef312374bd2abfbaa97f6ba
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-10-11 13:10:00 -0400

    Fixed an issue with question 1. Now everything is outputted as a vector

--- Assignment1/Assignment1-Q1.R
+++ Assignment1/Assignment1-Q1.R
@@ -1,21 +1,23 @@
+# I have left the bases as lower case characters because they seem to work with the seqinr package
+
 # Code for Part 1 of Question 1
 i <- 0
 #subSeq <- "ACTG"
 dnaSeq1 <- character()
 
 while (i < 100) { # The loop nuns 400 times because 4 bases x 100 = 400 base strand
-  dnaSeq1 <- append(dnaSeq1,"A")
-  dnaSeq1 <- append(dnaSeq1,"C")
-  dnaSeq1 <- append(dnaSeq1,"T")
-  dnaSeq1 <- append(dnaSeq1,"G")
+  dnaSeq1 <- append(dnaSeq1,"a")
+  dnaSeq1 <- append(dnaSeq1,"c")
+  dnaSeq1 <- append(dnaSeq1,"t")
+  dnaSeq1 <- append(dnaSeq1,"g")
   i = i+1
 }
-print(dnaSeq1)
-length(dnaSeq1)
+#print(dnaSeq1)
+#length(dnaSeq1)
 
 
 # Code for Part 2 of Question 1
-dnaSeq2 <- ""
+dnaSeq2 <- character()
 randomValGen <- sample(1:4, 400, replace=T) #Generates 400 random integers between 1-4 (representing each base)
 j <- 1
 #print(randomValGen)
@@ -23,26 +25,27 @@ j <- 1
 while(j<401){
   # Maps the generated random value to a base  
   if(randomValGen[j]==1){
-    dnaSeq2 = paste(dnaSeq2,'A',sep="")
+    dnaSeq2 <- append(dnaSeq2,"a")
   }
   else if(randomValGen[j]==2){
-    dnaSeq2 = paste(dnaSeq2,"C",sep="")
+    dnaSeq2 <- append(dnaSeq2,"c")
   }
   else if(randomValGen[j]==3){
-    dnaSeq2 = paste(dnaSeq2,"T",sep="")
+    dnaSeq2 <- append(dnaSeq2,"t")
   }
   else{
-    dnaSeq2 = paste(dnaSeq2,"G",sep="")
+    dnaSeq2 <- append(dnaSeq2,"g")
   }
   j = j+1
 }
-print(dnaSeq2)
+#count(dnaSeq2,1)
+#print(dnaSeq2)
 
 #print(nchar(dnaSeq2))
 
 
 # Code for Part 3 of Question 1
-dnaSeq3 <- ""
+dnaSeq3 <- character()
 k <- 1
 randVal=0;
   
@@ -53,7 +56,6 @@ while (k<601) {
   # goes to default cause of equal probabilities otherwise.
   if(k %% 3 ==0){
     #print(sprintf("%i is divisible by 3",k))
-    
     randVal = sample(c(1,2,3,4), 1, replace=TRUE, prob=c(0.5,0.25,0.15,0.1))
     #print(randVal)
     #print(sprintf("%i is divisible by 3",k))
@@ -63,19 +65,20 @@ while (k<601) {
   }
 
   if(randVal==1){
-    dnaSeq3 = paste(dnaSeq3,'A',sep="")
+    dnaSeq3 <- append(dnaSeq3,"a")
   }
   else if(randVal==2){
-    dnaSeq3 = paste(dnaSeq3,"C",sep="")
+    dnaSeq3 <- append(dnaSeq3,"c")
   }
   else if(randVal==3){
-    dnaSeq3 = paste(dnaSeq3,"T",sep="")
+    dnaSeq3 <- append(dnaSeq3,"t")
   }
   else{
-    dnaSeq3 = paste(dnaSeq3,"G",sep="")
+    dnaSeq3 <- append(dnaSeq3,"g")
   }
   k = k+1
 }
-print(dnaSeq3)
+#count(dnaSeq3,1)
+#print(dnaSeq3)
 
 #print(nchar(dnaSeq3))
