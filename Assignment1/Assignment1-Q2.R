780796390ce2683d5498bcf63d32d6352e9518db
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-10-11 02:03:43 -0400

    Part b done for Question 2

--- Assignment1/Assignment1-Q2.R
+++ Assignment1/Assignment1-Q2.R
@@ -7,3 +7,7 @@ dnaRaw1 <- read.fasta(file="sequence1.fasta") # Import fasta file
 seq1 <- getSequence(dnaRaw1[[1]], as.string = FALSE) # Extracts the nucleotides from fasta file
 count(seq1,1, freq = TRUE) # outputs the percentage values of the four bases that make up the sequence
 count(seq1,1, freq = FALSE) # outputs the freqency of bases
+
+
+# Part b
+count(seq1,2) # outputs the dimers for the sequence
