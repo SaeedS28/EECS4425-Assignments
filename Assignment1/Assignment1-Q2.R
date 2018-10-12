aa3fbfb14cc95dc1bb47ad5b2b77bd2e3ce785f9
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-10-12 01:46:34 -0400

    finished implementing question 3 and moved all the code to one file. Guess I'm done.

--- Assignment1/Assignment1-Q2.R
+++ Assignment1/Assignment1-Q2.R
@@ -1,7 +1,93 @@
 #install.packages("seqinr")
-# Question 2
 
-library(seqinr)
+# I have left the bases as lower case characters because they seem to work with the seqinr package
+
+# Code for Part 1 of Question 1
+i <- 0
+
+dnaSeq1 <- character()
+
+while (i < 100) { # The loop nuns 400 times because 4 bases x 100 = 400 base strand
+  dnaSeq1 <- append(dnaSeq1,"a")
+  dnaSeq1 <- append(dnaSeq1,"c")
+  dnaSeq1 <- append(dnaSeq1,"t")
+  dnaSeq1 <- append(dnaSeq1,"g")
+  i = i+1
+}
+#print(dnaSeq1)
+#length(dnaSeq1)
+
+
+# Code for Part 2 of Question 1
+dnaSeq2 <- character()
+randomValGen <- sample(1:4, 400, replace=T) #Generates 400 random integers between 1-4 (representing each base)
+j <- 1
+#print(randomValGen)
+
+while(j<401){
+  # Maps the generated random value to a base  
+  if(randomValGen[j]==1){
+    dnaSeq2 <- append(dnaSeq2,"a")
+  }
+  else if(randomValGen[j]==2){
+    dnaSeq2 <- append(dnaSeq2,"c")
+  }
+  else if(randomValGen[j]==3){
+    dnaSeq2 <- append(dnaSeq2,"t")
+  }
+  else{
+    dnaSeq2 <- append(dnaSeq2,"g")
+  }
+  j = j+1
+}
+#count(dnaSeq2,1)
+#print(dnaSeq2)
+
+#print(nchar(dnaSeq2))
+
+
+# Code for Part 3 of Question 1
+dnaSeq3 <- character()
+k <- 1
+randVal=0;
+
+while (k<601) {
+  
+  # this generates a random number based on the probabilities given in the question 
+  # sheet only when the number is divisible by three.
+  # goes to default cause of equal probabilities otherwise.
+  if(k %% 3 ==0){
+    #print(sprintf("%i is divisible by 3",k))
+    randVal = sample(c(1,2,3,4), 1, replace=TRUE, prob=c(0.5,0.25,0.15,0.1))
+    #print(randVal)
+    #print(sprintf("%i is divisible by 3",k))
+  }
+  else{
+    randVal = sample(c(1,2,3,4), 1, replace=TRUE)
+  }
+  
+  if(randVal==1){
+    dnaSeq3 <- append(dnaSeq3,"a")
+  }
+  else if(randVal==2){
+    dnaSeq3 <- append(dnaSeq3,"c")
+  }
+  else if(randVal==3){
+    dnaSeq3 <- append(dnaSeq3,"t")
+  }
+  else{
+    dnaSeq3 <- append(dnaSeq3,"g")
+  }
+  k = k+1
+}
+#count(dnaSeq3,1)
+#print(dnaSeq3)
+
+#print(nchar(dnaSeq3))
+
+# -----------------------------------------------------------------------------------------------------------
+
+# Question 2
 
 # Part a
 dnaRaw1 <- read.fasta(file="sequence1.fasta") # Import fasta file
@@ -17,7 +103,7 @@ count(seq1,2) # outputs the dimers for the sequence
 # Part c
 count(seq1,3) # outputs the dimers for the sequence
 
-
+# -----------------------------------------------------------------------------------------------------------
 
 # Question 3
 
@@ -52,7 +138,7 @@ extractAndIndicate <- function(seqInFunc, startIndex, endIndex){
     }
     counter <- counter + 1
   }
-  
+  counter <- 1
   # fast Fourier transform
   
   fftCoefficients <- fft(indicatorSequence)
@@ -65,7 +151,16 @@ extractAndIndicate(seqInFunc = seq1, startIndex = 1, endIndex = 12)
 
 
 # Part 2
-dnaRaw2 <- read.fasta(file="sequence2.fasta") # Import fasta file
+dnaRaw2 <- read.fasta(file="sequence2.fasta") # Import fasta file for 2nd sequence
 #print(dnaRaw2)
-seq2 <- getSequence(dnaRaw2[[1]], as.string = FALSE)
-print(seq2[4290:4784])
+codingSequence1 <- getSequence(dnaRaw2[[1]], as.string = FALSE)
+
+# According to the annotation file, the following range of values identify the area of the genome
+# that codes for a protien with protein_id=WP_010877517.1
+extractAndIndicate(seqInFunc = codingSequence1, startIndex = 4290, endIndex = 4784)
+
+# The following range of values identify the area of the genome that doesn't code for any protein
+extractAndIndicate(seqInFunc = codingSequence1, startIndex = 1, endIndex = 500)
+
+# The function is now used on the sequence generated for Question 1, part c
+extractAndIndicate(seqInFunc = dnaSeq3, startIndex = 1, endIndex = length(dnaSeq3))
