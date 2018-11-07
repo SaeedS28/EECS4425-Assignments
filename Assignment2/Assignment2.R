c9b2c778882309fc2083bc7d0b2de26043dbe188
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-11-07 01:58:27 -0500

    got the starting points to work. Now everything is truly randomized

--- Assignment2/Assignment2.R
+++ Assignment2/Assignment2.R
@@ -1,23 +1,37 @@
-placeholder <- integer()
+library('seqinr')
+
+# Question 1
+lengthExon <- integer()
 i <- 1
 
 # Puts the first value in the vector to get things going
-placeholder[i] <- sample(500:10000, replace=T)
+lengthExon[i] <- sample(500:10000, replace=T)
 
 # Now checks to see if the sum is still less than 100 000
-while(sum(placeholder) <= 100000){
+while(sum(lengthExon) <= 100000){
   i <- i+1
-  placeholder[i] <- sample(500:10000, replace=T)
+  lengthExon[i] <- sample(500:10000, replace=T)
 }
-
-print(sum(placeholder))
-
+i <- 1
 # If the sum is greater than 100 000, it subtracts the difference from the element with the highest value
 # to get things back to 100 000.
-maxIndex <- which.max(placeholder)
-print(placeholder[maxIndex])
+maxIndex <- which.max(lengthExon)
 
-difference <- sum(placeholder) - 100000 # Sum will always be greater than or equal to 100000
-#print(difference)
-placeholder[maxIndex] <- placeholder[maxIndex]-difference
-print(sum(placeholder))
+difference <- sum(lengthExon) - 100000 # Sum will always be greater than or equal to 100000
+lengthExon[maxIndex] <- lengthExon[maxIndex]-difference
+print(sprintf("Sum of the vector: %s", sum(lengthExon)))
+
+# Now randomize the process of finding the starting point of the exons
+
+startPoints <-integer()
+startPoints[i] <- sample(500:10000, replace=T)
+
+while(i <= length(lengthExon)){
+  i <- i+1
+  startPoints[i] <- startPoints[i-1]+lengthExon[i-1]+sample(12000:18000, replace=T)
+}
+
+print(sprintf("Length of startVector: %s  Length of exons: %s", length(startPoints), length(startPoints)))
+
+print(lengthExon)
+print(startPoints)
