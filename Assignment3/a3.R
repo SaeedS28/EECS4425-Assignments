0b8b9b9eb8203b7a45ec6a1a910333f972403974
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-11-29 15:09:45 -0500

    118th element is the charm

--- Assignment3/a3.R
+++ Assignment3/a3.R
@@ -1,6 +1,7 @@
 library('seqinr')
 library(stringr)
 
+# Question 1
 rawData <- readLines("sequenceAnnotation.txt")
 refinedData <- str_match(rawData,"location=[A-Za-z0-9$&+,:;=?@#|'<>-^*()%!]*\\.\\.[A-Za-z0-9$&+,:;=?@#|'<>-^*()%!]*") # regex used to find the locations in the annotation file
 refinedData <- refinedData[!is.na(refinedData)]
@@ -44,3 +45,50 @@ while(j<length(endfinalRefined-1)){
 }
 
 intronSequence <- ecoliSeq[endfinalRefined[differenceOverFiveThousandIntrons[1]]:startfinalRefined[differenceOverFiveThousandIntrons[1]+1]]
+
+
+indicatorGExon <- character()
+dnaCopyExon <- character()
+
+dnaCopyExon <- exonSequence[1:length(exonSequence)]
+dnaCopyExon <- tolower(dnaCopyExon)
+indicatorGExon <- dnaCopyExon
+
+# Replaces g's with ones and the rest with zeroes
+indicatorGExon[dnaCopyExon !='g'] <- 0
+indicatorGExon[dnaCopyExon =='g'] <- 1
+indicatorGExon <- as.numeric(indicatorGExon)
+
+
+indicatorGIntron <- character()
+dnaCopyIntron <- character()
+
+dnaCopyIntron <- intronSequence[1:length(intronSequence)]
+dnaCopyIntron <- tolower(dnaCopyIntron)
+indicatorGIntron <- dnaCopyIntron
+
+# Replaces g's with ones and the rest with zeroes
+indicatorGIntron[dnaCopyIntron !='g'] <- 0
+indicatorGIntron[dnaCopyIntron =='g'] <- 1
+indicatorGIntron <- as.numeric(indicatorGIntron)
+
+
+#Arg for fft exons
+window <- 351 #characters displayed in every read
+exonShift<- function(y) {
+  (Arg(fft((indicatorGExon[y:(y+window-1)]))))[118]
+}
+shiftValsExon <- seq(1,length(indicatorGExon),by=3) # shift 3 after every read for the length of the sequence
+
+readWindowsExon <- sapply(shiftValsExon,exonShift)
+readWindowsExon <- readWindowsExon[!is.na(readWindowsExon)] # deletes all the NA values from the data. Easier than figuring out the exact shiftVal sequence
+
+intronShift<- function(y) {
+  (Arg(fft((indicatorGIntron[y:(y+window-1)]))))[118]
+}
+shiftValsIntron <- seq(1,length(indicatorGIntron),by=3) # shift 3 after every read for the length of the sequence
+
+readWindowsIntron <- sapply(shiftValsIntron,intronShift)
+readWindowsIntron <- readWindowsIntron[!is.na(readWindowsIntron)] # deletes all the NA values from the data. Easier than figuring out the exact shiftVal sequence
+
+hist(readWindowsIntron)
